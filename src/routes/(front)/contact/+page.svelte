7d266626a6c4603dff811f7ee2407ca6cd744857
Matthew Roe <mr.roe7@gmail.com>
2023-10-03 11:12:15 -0500

    form fix attempt

--- src/routes/(front)/contact/+page.svelte
+++ src/routes/(front)/contact/+page.svelte
@@ -13,27 +13,33 @@
 Here you can inquire about us, our process and services, or just say hello!
 
 <Callout>We can share our phone number after we know we are speaking to a human, sorry for the inconvenience!</Callout>
+<div class="container mx-auto w-4/5">
+<form class="form-control" netlify name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true" netlify-honeypot="bot-field">
 
-<form class="form-control container mx-auto w-4/5" on:submit|preventDefault name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
-    <label>Your Name: <input placeholder="Name" class="input max-w-sm text-white" type="text" name="name" /></label>
-    <br>
-    <label>Business Name: <input placeholder="Business Name" class="input max-w-sm text-white" type="text" name="business" /></label>
-    <br>
+    <div class="grid grid-cols-2">
+      <label>Your Name: <input placeholder="Name" class="input max-w-sm text-white" type="text" name="name" /></label>
+
+      <label>Business Name: <input placeholder="Business Name" class="input max-w-sm text-white" type="text" name="business" /></label>
+
     <label>Your Email: <input placeholder="Email" class="input max-w-sm text-white" type="email" name="email" /></label>
-    <br>
+
     <label>Your Phone: <input placeholder="Phone" class="input max-w-sm text-white" type="phone" name="phone" /></label>
-    <br>
-    <label>Your Role: <br><select class="select w-full text-white w-fit" name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Individual Contributor</option>
-    </select></label>
     <br>
     <label class="cursor-pointer label w-fit">
       <input type="checkbox" checked="checked" class="checkbox checkbox-secondary" />
       <span class="label-text mx-5">I would rather speak on the phone next time.</span>
     </label>
+
+  </div>
+
+    <label>Your Role: <br><select class="select w-full text-white w-fit" name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Individual Contributor</option>
+    </select></label>
     <br>
+
     <label>Message: <br><textarea class="textarea textarea-bordered text-white w-full" placeholder="Message" name="message"></textarea></label>
     <br>
     <button class="btn btn-secondary" type="submit">Send</button>
 </form>
+</div>
