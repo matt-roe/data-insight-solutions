d445d71274771cb7ae434d0e6d5ddecafa59d341
Matthew Roe <mr.roe7@gmail.com>
2023-10-03 14:50:49 -0500

    Update landing carousel images

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,29 +1,22 @@
-<div class="carousel carousel-center gap-10 -mt-[10rem] lg:-mt-[10rem] py-10">
+<div class="grid grid-cols-3 mx-auto w-full carousel carousel-center gap-10 -mt-[10rem] lg:-mt-[10rem] py-10 ">
 	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
 		<img
-			class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-			src="/images/C.png"
+			class="w-48 sm:w-48 lg:w-64 xl:w-96 rounded-lg shadow-lg"
+			src="/images/collab.png"
 			alt="Burger"
 		/>
 	</div>
 	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
 		<img
-			class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-			src="/images/A.png"
+			class="w-48 sm:w-48 lg:w-64 xl:w-96 rounded-lg shadow-lg"
+			src="/images/contemplateLayers.png"
 			alt="Burger"
 		/>
 	</div>
 	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
 		<img
-			class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-			src="/images/I.png"
-			alt="Burger"
-		/>
-	</div>
-	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<img
-			class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-			src="/images/D.png"
+			class="w-48 sm:w-48 lg:w-64 xl:w-96 rounded-lg shadow-lg"
+			src="/images/datacenter.png"
 			alt="Burger"
 		/>
 	</div>
