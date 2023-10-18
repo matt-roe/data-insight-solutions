b851f3d27a1be70f837d4ddb34c027c545cadd3b
matt-roe <mr.roe7@gmail.com>
2023-10-18 14:28:09 -0500

    work email for contact form & services edit

--- src/routes/(front)/contact/+page.svelte
+++ src/routes/(front)/contact/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-  import { Input, Select, Label, Helper, Button, Checkbox, A, Textarea} from 'flowbite-svelte';
-  let selected;
-  let options = [
-    { value: 'lead', name: 'Leader' },
-    { value: 'contributor', name: 'Contributor' }
-  ];
+	import { Input, Select, Label, Helper, Button, Checkbox, A, Textarea } from 'flowbite-svelte';
+	let selected;
+	let options = [
+		{ value: 'lead', name: 'Leader' },
+		{ value: 'contributor', name: 'Contributor' }
+	];
 </script>
 
 <svelte:head>
@@ -15,16 +15,45 @@
 
 Here you can inquire about us, our process and services, or just say hello!
 
-<div >
-<form netlify id="contactForm" class="grid grid-flow-row gap-6" name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true" netlify-honeypot="bot-field" action="https://formspree.io/f/mleyowkw">
-  <Label for="name" class="mb-2 max-w-sm min-w-xs">Your Name: <Input id="name" placeholder="Name" type="text" name="name" /></Label>
-  <Label for="business" class="mb-2 max-w-sm min-w-xs">Business Name: <Input id="business" placeholder="Business Name"  type="text" name="business" /></Label>
-  <Label for="email" class="mb-2 max-w-sm min-w-xs">Your Email: <Input id="email" placeholder="Email" type="email" name="email" /></Label>
-  <Label for="phone" class="mb-2 max-w-sm min-w-xs">Your Phone: <Input id="phone" placeholder="Phone"  type="phone" name="phone" /></Label>
-  <Checkbox id="preferPhone" class="mb-2 max-w-sm min-w-xs" name="preferPhone"> I prefer phone.</Checkbox>
-  <Label for="role" class="mb-2 max-w-sm min-w-xs">Your Role: <br><Select class="mt-2" items={options} bind:value={selected} /></Label>
-  <Label for="message" class="mb-2 md:col-span-3">Message: <br><Textarea id="message" required placeholder="Message" name="message"></Textarea></Label>
-  <Input type="hidden" name="form-name" value="contact" />
-  <Button class="btn btn-secondary mx-auto" type="submit" value="Submit">Send</Button>
-</form>
+<div>
+	<form
+		netlify
+		id="contactForm"
+		class="grid grid-flow-row gap-6"
+		name="contact"
+		method="POST"
+		data-netlify="true"
+		data-netlify-recaptcha="true"
+		netlify-honeypot="bot-field"
+		action="https://formspree.io/f/mvojdjre"
+	>
+		<Label for="name" class="mb-2 max-w-sm min-w-xs"
+			>Your Name: <Input id="name" placeholder="Name" type="text" name="name" /></Label
+		>
+		<Label for="business" class="mb-2 max-w-sm min-w-xs"
+			>Business Name: <Input
+				id="business"
+				placeholder="Business Name"
+				type="text"
+				name="business"
+			/></Label
+		>
+		<Label for="email" class="mb-2 max-w-sm min-w-xs"
+			>Your Email: <Input id="email" placeholder="Email" type="email" name="email" /></Label
+		>
+		<Label for="phone" class="mb-2 max-w-sm min-w-xs"
+			>Your Phone: <Input id="phone" placeholder="Phone" type="phone" name="phone" /></Label
+		>
+		<Checkbox id="preferPhone" class="mb-2 max-w-sm min-w-xs" name="preferPhone">
+			I prefer phone.</Checkbox
+		>
+		<Label for="role" class="mb-2 max-w-sm min-w-xs"
+			>Your Role: <br /><Select class="mt-2" items={options} bind:value={selected} /></Label
+		>
+		<Label for="message" class="mb-2 md:col-span-3"
+			>Message: <br /><Textarea id="message" required placeholder="Message" name="message" /></Label
+		>
+		<Input type="hidden" name="form-name" value="contact" />
+		<Button class="btn btn-secondary mx-auto" type="submit" value="Submit">Send</Button>
+	</form>
 </div>
--- src/routes/(front)/services/+page.md
+++ src/routes/(front)/services/+page.md
@@ -7,14 +7,14 @@
 
 Technology solutions are widely varying in complexity and scope. For this reason we set starting costs for services. This is to allow for a successful service, even when the goal is a large one. Generally services are priced at $200 per hour.
 
-## Consulting
+## Consultation
 
 - Macro Process Diagramming
 
 Starting at $400
 <br>
 
-- Solution Design & resulting Statement of Work
+- Solution Design & Statement of Work
 
 Starting at $800
 <br>
@@ -34,7 +34,7 @@ Starting at $600
 $300 per hour
 <br>
 
-- Business Start-Up Technology Strategy
+- Start-Up Technology Strategy
 
 $250 per hour
 <br>
